2cc54a4e0b1a9a47ff23876ff53eae2af590d38d
b02247 <b02247@freescale.com>
2010-05-28 14:30:08 +0800

    add FLAC

--- media/java/android/media/MediaFile.java
+++ media/java/android/media/MediaFile.java
@@ -184,6 +184,8 @@ public class MediaFile {
         addFileType("AAC", FILE_TYPE_AAC, "audio/aac-adts", MtpConstants.FORMAT_AAC);
         addFileType("MKA", FILE_TYPE_MKA, "audio/x-matroska");
  
+        addFileType("FLAC", FILE_TYPE_FLAC, "audio/flac");
+         
         addFileType("MID", FILE_TYPE_MID, "audio/midi");
         addFileType("MIDI", FILE_TYPE_MID, "audio/midi");
         addFileType("XMF", FILE_TYPE_MID, "audio/midi");
