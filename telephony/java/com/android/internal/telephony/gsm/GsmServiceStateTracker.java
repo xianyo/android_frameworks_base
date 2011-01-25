45d2cb2ab6ca1ec6ae948a7e6089e02826ff9934
Zhang Jiejing <jiejing.zhang@freescale.com>
2011-01-25 13:34:38 +0800

    ENGR00138429 add report GPRS type in CREG's return
    Some modem(Amazon1) will report network type in CREG,
    Add this in Android frameworks.
    
    Signed-off-by: Zhang Jiejing <jiejing.zhang@freescale.com>

--- telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -563,6 +563,7 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                     int regState = -1;
                     int reasonRegStateDenied = -1;
                     int psc = -1;
+                    int Act = 0;
                     if (states.length > 0) {
                         try {
                             regState = Integer.parseInt(states[0]);
@@ -581,6 +582,11 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
                             }
                         } catch (NumberFormatException ex) {
                             loge("error parsing RegistrationState: " + ex);
+                            // Act(type) may reported in +CREG?
+                            // Not only in CGREG.
+                            if (states.length >= 4
+                                && states[3] != null && states[3].length() > 0)
+                                Act = Integer.parseInt(states[3], 16);
                         }
                     }
 
@@ -595,7 +601,9 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
 
                     // LAC and CID are -1 if not avail
                     newCellLoc.setLacAndCid(lac, cid);
-                    newCellLoc.setPsc(psc);
+                    newCellLoc.sqetPsc(psc);
+                    newNetworkType = Act;
+                    newSS.setRadioTechnology(Act);
                 break;
 
                 case EVENT_POLL_STATE_GPRS:
