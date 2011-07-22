5b97ce44ef53c4292ce7f76269de5c9db601af21
Song Bing <b06498@freescale.com>
2011-07-22 17:05:10 +0800

    ENGR00133115 Update OMX recorder set property.
    Update OMX recorder set property.
    
    Signed-off-by: Song Bing <b06498@freescale.com>

--- media/libmediaplayerservice/MediaRecorderClient.cpp
+++ media/libmediaplayerservice/MediaRecorderClient.cpp
@@ -296,8 +296,8 @@ MediaRecorderClient::MediaRecorderClient(const sp<MediaPlayerService>& service, 
     mPid = pid;
 
     char value[PROPERTY_VALUE_MAX];
-    if (!property_get("media.omxgm.enable-record", value, NULL)
-        || !strcmp(value, "1") || !strcasecmp(value, "true")) {
+    if (property_get("media.omxgm.enable-record", value, NULL)
+        && ( !strcmp(value, "1") || !strcasecmp(value, "true"))) {
         mRecorder = new OMXRecorder;
     } else if (!property_get("media.stagefright.enable-record", value, NULL)
         || !strcmp(value, "1") || !strcasecmp(value, "true")) {
