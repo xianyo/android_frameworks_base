36cf49f6a3087e1b3703db6ecd86aa4824900623
YangYonggang <b31664@freescale.com>
2011-11-23 19:33:03 +0800

    Revert "Remove the reponse of the battery service event, since we dont have a fully implementation battery control Signed-off-by: b07211 <b07211@freescale.com>"
    This reverts commit 8250632118908b52f3b12c05445261477136e8ae.

--- packages/SystemUI/src/com/android/systemui/usb/UsbStorageActivity.java
+++ packages/SystemUI/src/com/android/systemui/usb/UsbStorageActivity.java
@@ -196,15 +196,8 @@ public class UsbStorageActivity extends Activity
     private void handleUsbStateChanged(Intent intent) {
         boolean connected = intent.getExtras().getBoolean(UsbManager.USB_CONNECTED);
         if (!connected) {
-        }
-    }
-
-    private void handleBatteryChanged(Intent intent) {
-        int pluggedType = intent.getIntExtra("plugged", 0);
-        if (pluggedType == 0) {
-            Log.i(TAG, "Warning!handleBatteryChanged******");
             // It was disconnected from the plug, so finish
-            //finish();
+            finish();
         }
     }
 
