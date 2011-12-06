b6193d30bdc94a56cb0f418019d64270a364ab92
b07117 <b07117@freescale.com>
2011-12-06 23:47:50 +0800

    ENGR00163695 Integrate gpu hwcomposer into ics
    gpu hwcomposer depend on egl wrapper enhancement
    
    Signed-off-by: Li Xianzhong <b07117@freescale.com>

--- opengl/include/EGL/eglext.h
+++ opengl/include/EGL/eglext.h
@@ -229,6 +229,14 @@ struct ANativeWindowBuffer;
 #define EGL_NATIVE_BUFFER_ANDROID               0x3140  /* eglCreateImageKHR target */
 #endif
 
+#ifndef EGL_ANDROID_get_render_buffer
+#define EGL_ANDROID_get_render_buffer 1
+#ifdef EGL_EGLEXT_PROTOTYPES
+EGLAPI EGLClientBuffer EGLAPIENTRY eglGetRenderBufferANDROID(EGLDisplay dpy, EGLSurface draw);
+#endif
+typedef EGLClientBuffer (EGLAPIENTRYP PFNEGLGETRENDERBUFFERANDROIDPROC) (EGLDisplay dpy, EGLSurface draw);
+#endif
+
 #ifndef EGL_ANDROID_recordable
 #define EGL_ANDROID_recordable 1
 #define EGL_RECORDABLE_ANDROID                  0x3142  /* EGLConfig attribute */
