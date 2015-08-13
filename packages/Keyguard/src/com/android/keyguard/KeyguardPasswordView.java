e60092bcc11c31b7693c8d9c1d9f68cbfe80a753
Wenbo Zhu <b52619@freescale.com>
2015-08-13 11:32:40 +0800

    MA-6943 [Android_MX7D_SD] GUI: No string displayed if set Screen Lock password (HDMI display)
    add destroyDrawingCache() to refresh UI.
    
    Signed-off-by: Wenbo Zhu <b52619@freescale.com>

--- packages/Keyguard/src/com/android/keyguard/KeyguardPasswordView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPasswordView.java
@@ -282,6 +282,7 @@ public class KeyguardPasswordView extends KeyguardAbsKeyInputView
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         if (mCallback != null) {
+            destroyDrawingCache();
             mCallback.userActivity();
         }
     }
